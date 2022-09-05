ac123e06a4b7d07588d973f471f9b00163a0c7d8
Rob Cobb <rwcobbjr@gmail.com>
2022-09-05 11:25:17 -0600

    solve

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-# Write your solution below.
-
 # Create a list of your squad members
 
+squad = ["Keno", "Emmy", "Rob", "Mohammed", "Seun", "Ope"]
+
+print("Squad 19, the flying snails:")
+
 # Use a loop to print each name
+
+for name in squad:
+    print(name)
